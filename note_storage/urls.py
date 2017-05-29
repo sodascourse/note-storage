1db3c157862e3b9bb6facb545f88d1022af51cfb
sodastsai <sodas2002@gmail.com>
2017-05-30 00:31:59 +0800

    Add 404 json error page

--- note_storage/urls.py
+++ note_storage/urls.py
@@ -1,7 +1,17 @@
+from http import HTTPStatus
+
 from django.conf.urls import url, include
+from django.http import JsonResponse
 
 from auth_api.urls import urlpatterns as auth_api_urls
 from simple_note.router import note_router
+
+
+def handler404(request):
+    resp = JsonResponse({"path": request.path, "message": "Not found."})
+    resp.status_code = HTTPStatus.NOT_FOUND
+    return resp
+
 
 urlpatterns = [
     *auth_api_urls,
